93bb954f04debae12db536992bca9809875d01e2
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-05-14 02:34:48 +0530

    update changelog

--- src/app/models/screenshots.ts
+++ src/app/models/screenshots.ts
@@ -3,17 +3,17 @@ export class ScreenShot {
 }
 
 export const ScreenShots: ScreenShot[] = [
-    new ScreenShot("Light Theme", "http://i.imgur.com/syPGnSd.png"),
-    new ScreenShot("Dark Theme", "http://i.imgur.com/BHZBhoU.png"),
-    new ScreenShot("Customize Mouse Clicks and Keystrokes", "http://i.imgur.com/SpUpDC1.png"),
-    new ScreenShot("Configure | Options", "http://i.imgur.com/MLARQ0P.png"),
-    new ScreenShot("Configure | Hotkeys", "http://i.imgur.com/4mE6rXO.png"),
-    new ScreenShot("Configure | FFMpeg", "http://i.imgur.com/e06zmlC.png"),
-    new ScreenShot("Configure | Proxy", "http://i.imgur.com/MaaSOnP.png"),
-    new ScreenShot("Configure | Extras", "http://i.imgur.com/03nUBPi.png"),
-    new ScreenShot("WebCam View", "http://i.imgur.com/5zgBov2.png"),
-    new ScreenShot("FFMpeg Log", "http://i.imgur.com/sAY0dya.png"),
-    new ScreenShot("FFMpeg Downloader", "http://i.imgur.com/PY4LtiY.png"),
-    new ScreenShot("Recent", "http://i.imgur.com/J0htLts.png"),
-    new ScreenShot("About", "http://i.imgur.com/tDz0Wgw.png")
+    new ScreenShot("Light Theme", "https://i.imgur.com/syPGnSd.png"),
+    new ScreenShot("Dark Theme", "https://i.imgur.com/BHZBhoU.png"),
+    new ScreenShot("Customize Mouse Clicks and Keystrokes", "https://i.imgur.com/SpUpDC1.png"),
+    new ScreenShot("Configure | Options", "https://i.imgur.com/MLARQ0P.png"),
+    new ScreenShot("Configure | Hotkeys", "https://i.imgur.com/4mE6rXO.png"),
+    new ScreenShot("Configure | FFMpeg", "https://i.imgur.com/e06zmlC.png"),
+    new ScreenShot("Configure | Proxy", "https://i.imgur.com/MaaSOnP.png"),
+    new ScreenShot("Configure | Extras", "https://i.imgur.com/03nUBPi.png"),
+    new ScreenShot("WebCam View", "https://i.imgur.com/5zgBov2.png"),
+    new ScreenShot("FFMpeg Log", "https://i.imgur.com/sAY0dya.png"),
+    new ScreenShot("FFMpeg Downloader", "https://i.imgur.com/PY4LtiY.png"),
+    new ScreenShot("Recent", "https://i.imgur.com/J0htLts.png"),
+    new ScreenShot("About", "https://i.imgur.com/tDz0Wgw.png")
 ];
