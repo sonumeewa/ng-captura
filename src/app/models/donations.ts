d5ba11300d4fa9c73f5f8ff686a3a37219479327
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-05-18 02:29:53 +0530

    update donations

--- src/app/models/donations.ts
+++ src/app/models/donations.ts
@@ -7,5 +7,6 @@ export const DonationHistory: Donation[] = [
     new Donation("GOFF Concepts L.L.C.", "$25 USD", "http://goffconcepts.com"),
     new Donation("George van der Ven", "$10 USD"),
     new Donation("Geoffroy Menard", "$15 CAD"),
-    new Donation("Ian Harpur", "1,000 RUB")
+    new Donation("Ian Harpur", "1,000 RUB"),
+    new Donation("Christian Brüggemann", "€ 5.00 EUR")
 ];
