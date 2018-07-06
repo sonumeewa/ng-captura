6e01714ac9e1fad1aad4b0e1d010563abe54b123
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-07-06 14:18:34 +0530

    update faq

--- src/app/models/donations.ts
+++ src/app/models/donations.ts
@@ -9,5 +9,6 @@ export const DonationHistory: Donation[] = [
     new Donation("Geoffroy Menard", "$15 CAD"),
     new Donation("Ian Harpur", "1,000 RUB"),
     new Donation("Christian Brüggemann", "€ 5.00 EUR"),
-    new Donation("Grisha Konzalaev", "300 RUB")
+    new Donation("Grisha Konzalaev", "300 RUB"),
+    new Donation("--", "£20.00 GBP")
 ];
